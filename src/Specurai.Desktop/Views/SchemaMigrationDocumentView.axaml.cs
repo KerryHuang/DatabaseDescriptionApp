e3479dbc55c09d087823b2f8928f6e38c2d4e257
Kerry Huang <kerry.huang@wdmis.com.tw>
2026-04-20 17:51:30 +0800

    feat: 新增 SchemaMigrationDocumentView AXAML 介面

---
+++ src/Specurai.Desktop/Views/SchemaMigrationDocumentView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Specurai.Desktop.Views;
+
+public partial class SchemaMigrationDocumentView : UserControl
+{
+    public SchemaMigrationDocumentView()
+    {
+        InitializeComponent();
+    }
+}
